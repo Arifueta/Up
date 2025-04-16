ba06ac830c9f0824a60acf065bf1b66dce0ca389
Arifueta <148445334+Arifueta@users.noreply.github.com>
2025-04-16 10:53:05 +0500

    Delete UPApp directory

--- UPApp/UPApp/Models/Service.cs
+++
@@ -1,9 +0,0 @@
-namespace UPApp.Models
-{
-    public class Service
-    {
-        public int Code { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-} 
